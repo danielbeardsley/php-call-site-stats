6480438c730d727c88edfbbea615523684e38f97
Daniel Beardsley <daniel@ifixit.com>
2013-06-10 10:38:24 -0700

    Summarize: calculate stats as-we-go
    Instead of loading the whole file into memory, calculate everything one
    line at a time.
    
    Most of these formulas are accurate enough with the kind of data we're
    talking about. They can get off when dealing with large discrepancies in
    magnitude.

--- summarize.php
+++ summarize.php
@@ -2,18 +2,47 @@
 
 $options = getopt('f:',['ratio:','stats:']);
 $filename = $options['f'];
-$stats = new StatsCollection(fopen($filename,'r'));
 if (isset($options['stats'])) {
    $column = $options['stats'];
-   $stats->printStats(intval($column) - 1);
+   $stats = new StatsCollection('stats', intval($column));
 } else if (isset($options['ratio'])) {
    $columns = explode(':',$options['ratio']);
-   $stats->printRatios(intval($columns[0])-1,intval($columns[1])-1);
+   $stats = new StatsCollection('ratio',
+                                intval($columns[0]),
+                                intval($columns[1]));
+} else {
+   echo <<<HELP
+Aggregate call-site stats recorded by CallSiteStats.php input columns
+are expected to be space-delimited. Lines are grouped by column 1.
+
+Usage:
+   summarize --stats=2 -f stats-input.log
+   summarize --ratio=3:4 -f stats-input.log
+
+Options:
+   -f             Input filename (required)
+   --stats        Calculate min/max/mean/std .. for a given column
+   --ratio=x:y    Total and calculate ratio between column x and column y
+HELP;
+   exit(1);
 }
+
+$stats->readLines(fopen($filename,'r'));
+$stats->printResults();
 
 
 class StatsCollection {
-   public function __construct($inStream) {
+   public function __construct($type, $column1, $column2 = null) {
+      $this->stats = $type == 'stats';
+      $this->ratio = $type == 'ratio';
+      $this->column1 = $column1;
+      if ($this->ratio) {
+         $this->column2 = $column2;
+      }
+   }
+
+
+   public function readLines($inStream) {
       while ($line = fgets($inStream)) {
          $this->addLine($line);
       }
@@ -22,47 +51,100 @@ class StatsCollection {
 
    protected function addLine($line) {
       $parts = explode(" ", $line);
-      $this->lines[$parts[0]][] = array_slice($parts, 1);
-   }
-
-   public function printRatios($numerator, $denominator) {
-      $firstLine = reset($this->lines);
-      $blankSumRow = array_fill(0,count($firstLine[0]),0);
-      foreach($this->lines as $key => $rows) {
-         $sums = $blankSumRow;
-         foreach($rows as $rowIndex => $row) {
-            foreach($row as $i => $value) {
-               $sums[$i] += $value;
-            }
+      $lineData = &$this->lines[$parts[0]];
+      if ($this->stats) {
+         if ($lineData === null){ 
+            $lineData = new StatsStream();
          }
-         $ratio = round(100 * ($sums[$numerator] / $sums[$denominator]), 2);
-         echo "$key {$sums[$numerator]} / {$sums[$denominator]} = $ratio%\n";
+         $lineData->write($parts[$this->column1]);
+      } else if ($this->ratio) {
+         if ($lineData === null){ 
+            $lineData = [0,0];
+         }
+         $lineData[0] += $parts[$this->column1];
+         $lineData[1] += $parts[$this->column2];
       }
    }
 
-   public function printStats($column) {
-      foreach($this->lines as $key => $rows) {
-         $sum = 0;
-         $min = null;
-         $max = null;
-         $values = [];
-         foreach($rows as $rowIndex => $row) {
-            $values[] = floatval($row[$column]); 
-         }
-         $min = min($values);
-         $max = max($values);
-         $sum = array_sum($values);
-         $count = count($values);
-         $avg = $sum / $count;
-         $sumOfSquares = 0;
-         foreach($values as $value) {
-            $sumOfSquares += $value * $value;
-         }
-         $std = sqrt(($sumOfSquares / count($values)) - $avg*$avg);
+   public function printResults() {
+      if ($this->stats) {
+         $this->printStats();
+      } else if ($this->ratio) {
+         $this->printRatios();
+      }
+   }
+
+   public function printRatios() {
+      foreach($this->lines as $key => $stats) {
+         $num = $stats[0];
+         $den = $stats[1];
+         $pct = round(100 * ($num / $den), 2);
+         echo "$key $num / $den = $pct%\n";
+      }
+   }
+
+   public function printStats() {
+      foreach($this->lines as $key => $stats) {
+         $sum = $stats->sum();
+         $min = $stats->min();
+         $max = $stats->max();
+         $count = $stats->n();
+         $avg = $stats->mean();
+         $std = $stats->standard_deviation();
          $std = round($std, 3);
          $avg = round($avg, 3);
          echo "$key avg:$avg count:$count sum:$sum std:$std min:$min max:$max\n";
       }
+   }
+}
+
+class StatsStream {
+   public function __construct() {
+      $this->_min = null;
+      $this->_max = null;
+      // number of items seen
+      $this->_n = 0;
+      // running mean
+      $this->_mean = 0;
+      // running sum of squares deviations from the mean
+      $this->_ss = 0;
+      // the running 'actual sum'
+      $this->_sum = 0;
+      $this->writable = true;
+   }
+
+   public function write($x) {
+      $x = floatval($x);
+
+      $old_n = $this->_n++;
+
+      if ($old_n === 0) {
+          $this->_min  = $x;
+          $this->_max  = $x;
+          $this->_mean = $x;
+          $this->_sum  = $x;
+      } else {
+          if ($x < $this->_min) $this->_min = $x;
+          if ($x > $this->_max) $this->_max = $x;
+          $xdiff = $x - $this->_mean;
+          $this->_ss += $old_n * $xdiff * $xdiff / $this->_n;
+          $this->_mean += ($x - $this->_mean) / $this->_n;
+          $this->_sum += $x;
+      }
+   }
+
+   public function n() { return $this->_n; }
+   public function min() { return $this->_min; }
+   public function max() { return $this->_max; }
+   public function sum() { return $this->_sum; }
+   public function mean() { return $this->_mean; }
+
+   public function variance() {
+       return $this->_ss / $this->_n;
+   }
+
+   public function standard_deviation() {
+       return sqrt($this->variance());
    }
 }
 
